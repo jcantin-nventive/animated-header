fdd757ad3467dd48319bf013a2a08593087b10cb
Julie Cantin <julie.cantin@nventive.com>
2020-01-03 17:18:32 -0500

    currently works correctly on android

---
+++ AnimatedHeaderTest/AnimatedHeaderTest.Shared/MyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AnimatedHeaderTest
+{
+	public class MyViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private double _relativeOffset;
+		public double RelativeOffset
+		{
+			get { return _relativeOffset; }
+			set
+			{
+				_relativeOffset = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(RelativeOffset)));
+			}
+		}
+	}
+}
